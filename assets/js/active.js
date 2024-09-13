b1632b20774eab3ee7ce8aa4c20322e67e6bd759
emondesigner <eroy6854@gamil.com>
2024-09-13 13:23:45 +0600

    Home page Responsive 1

--- assets/js/active.js
+++ assets/js/active.js
@@ -9,6 +9,33 @@ $('.brands_slider').slick({
     arrows:false,
     infinite:true,
     slidesToScroll:1,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint:768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     
 });
 // .................................. CAR  BRANDS  SLIDER SECTION END..................................
@@ -22,6 +49,34 @@ $('.style_slider').slick({
     arrows:false,
     infinite:true,
     slidesToScroll:1,
+    
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint:768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     
 });
 //  ..................................CAR  STYLE  SLIDER SECTION END .................................. 
